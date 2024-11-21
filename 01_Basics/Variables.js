55b46cdef53fdbd58a9e1554a7763ab98b9b583a
Shivam-Sharma855 <shivamsharmabp610@gmail.com>
2024-11-21 07:09:21 +0000

    Variables Done

---
+++ 01_Basics/Variables.js
@@ -0,0 +1,10 @@
+const accountId= 144553
+let accountEmail="Shivam@google.com"
+var accountPassword="12345"
+accountCity="Yamunanagar"
+
+// accountId=55 not allowed
+accountEmail="Shiva@google.com"
+accountPassword="145"
+accountCity="nagar"
+console.table([accountEmail,accountPassword,accountCity])
