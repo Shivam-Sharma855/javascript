ad9731e5c181c3565914e4d6c07c66982243969b
Shivam-Sharma855 <shivamsharmabp610@gmail.com>
2024-11-22 06:50:21 +0000

    DataTypes Done

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all js code as a newer js version
+alert(3+3) // we are using nodejs, not browser
+console.log(3
+    +
+    3); //code redability should be high
+ console.log("Shivam Sharma"); // print Shivam Sharma
+ let name="Shivam Sharma" // String DataType
+ let age=21 // Number DataType
+ let isLoggedIn= true //Boolean Datatype
+ let state= null // object
+ 
+// number => 2 to power 53
+//bigint => when i use 2 to power 53 large number
+//string => ""
+//boolean => true or false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+console.log(typeof undefined); //undefined
+console.log(typeof null);// object
+
