ca3573c99b20ae5dc1e2c799511c8f1b50b271ab
Shivam-Sharma855 <shivamsharmabp610@gmail.com>
2024-11-22 08:16:39 +0000

    done

---
+++ 01_Basics/04_arithmeticOperator.js
@@ -0,0 +1,17 @@
+//Arithmetic Operator 
+// addition operator
+let a = 4;
+let b = 5;
+console.log("a =",a, "& b =",b ); // print a=4 b=5
+
+console.log(a+b); // first method ouput= 9
+console.log("a + b =", a + b);// second method output= a + b = 9
+//substraction operator
+console.log(a - b);
+console.log("a - b =", a - b);
+//multiplication
+console.log(a * b);
+console.log("a * b =", a * b);
+//dividetion
+console.log(a / b);
+console.log("a / b =", a / b);
